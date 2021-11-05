2877a6d57c4185384622cb10ad940c4056d2d3f2
Jordi Bruin <jordi@goodsnooze.com>
2021-11-05 13:51:40 +0100

    Initial commit

---
+++ Cooldown/RemoteConfig.swift
@@ -0,0 +1,8 @@
+//
+//  RemoteConfig.swift
+//  Cooldown
+//
+//  Created by Jordi Bruin on 05/11/2021.
+//
+
+import Foundation
---
+++ Cooldown/StatusBar.swift
@@ -0,0 +1,20 @@
+//
+//  StatusBar.swift
+//  Cooldown
+//
+//  Created by Jordi Bruin on 05/11/2021.
+//
+
+import SwiftUI
+
+struct StatusBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StatusBar_Previews: PreviewProvider {
+    static var previews: some View {
+        StatusBar()
+    }
+}
