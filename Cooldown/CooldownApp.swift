d934307a03f73463f879691162201546a44f68b4
Jordi Bruin <jordi@goodsnooze.com>
2021-11-05 11:31:40 +0100

    Initial Commit

---
+++ Cooldown/CooldownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CooldownApp.swift
+//  Cooldown
+//
+//  Created by Jordi Bruin on 05/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct CooldownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
